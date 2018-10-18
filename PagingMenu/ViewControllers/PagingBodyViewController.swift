6bd2366aabe3df83a52334b6a986497230b2a22c
takuyama29 <tmn.sby001@gmail.com>
2018-10-18 15:40:33 +0900

    スクロール用のストーリーボードを追加

---
+++ PagingMenu/ViewControllers/PagingBodyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PagingBodyViewController.swift
+//  PagingMenu
+//
+//  Created by 山田卓 on 2018/10/18.
+//  Copyright © 2018 TakuYamada. All rights reserved.
+//
+
+import UIKit
+
+class PagingBodyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
