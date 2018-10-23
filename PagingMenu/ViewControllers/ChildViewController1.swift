2d16b9865427f9edc8953e918ab8917bec67b729
takuyama29 <tmn.sby001@gmail.com>
2018-10-23 22:03:22 +0900

    デバッグコード追加

--- PagingMenu/ViewControllers/ChildViewController1.swift
+++ PagingMenu/ViewControllers/ChildViewController1.swift
@@ -10,21 +10,10 @@ import UIKit
 
 class ChildViewController1: UIViewController {
 
+    @IBAction func button(_ sender: UIButton) {
+        print("ChildViewController1 button Tap!")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- PagingMenu/ViewControllers/ChildViewController2.swift
+++ PagingMenu/ViewControllers/ChildViewController2.swift
@@ -10,21 +10,10 @@ import UIKit
 
 class ChildViewController2: UIViewController {
 
+    @IBAction func button(_ sender: UIButton) {
+        print("ChildViewController2 button Tap!")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
