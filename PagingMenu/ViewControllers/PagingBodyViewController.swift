a46b76beef5d19da9e3def9cf7cbd4640f6a352e
takuyama29 <tmn.sby001@gmail.com>
2018-10-23 21:47:04 +0900

    PagingBodyBiewControllerの大枠を実装

--- PagingMenu/ViewControllers/PagingBodyViewController.swift
+++ PagingMenu/ViewControllers/PagingBodyViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class PagingBodyViewController: UIViewController {
-    @IBOutlet private weak var horizontalStackView: UIStackView!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    
     private var container: [UIViewController] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        container.forEach { viewController in
-            horizontalStackView.addArrangedSubview(viewController.view)
+        container.enumerated().forEach { (index, viewController) in
+            setSubView(with: viewController.view, index: index)
         }
+        scrollView.contentSize = CGSize(width: scrollView.frame.width * container.count.toCGFloat,
+                                        height: scrollView.frame.height)
     }
     
     func configure(with container: [UIViewController]) {
         self.container = container
+    }
+    
+    func setSubView(with view: UIView, index: Int) {
+        view.bounds.origin = CGPoint(x: scrollView.frame.width * index.toCGFloat, y: 0)
+        scrollView.addSubview(view)
     }
 }
