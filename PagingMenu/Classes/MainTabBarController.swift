0b51052f4ae16c7405f595069cd365e70802658d
takuyama29 <tmn.sby001@gmail.com>
2018-11-01 16:21:30 +0900

    タブバーのレイアウトを修正

--- PagingMenu/Classes/MainTabBarController.swift
+++ PagingMenu/Classes/MainTabBarController.swift
@@ -12,7 +12,9 @@ import UIKit
 class MainTabBarController: UITabBarController {
     private var fixedNumberPagingViewController: UIViewController {
         let storyboard = UIStoryboard(name: FixedNumberPagingViewController.identifier, bundle: nil)
-        return storyboard.instantiateInitialViewController()!
+        let viewController = storyboard.instantiateInitialViewController()!
+        viewController.tabBarItem = UITabBarItem(title: "Fixed", image: UIImage(named: "fixed_icon"), tag: 0)
+        return viewController
     }
     
     override func viewDidLoad() {
